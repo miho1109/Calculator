8a846bd40510928cbc0968ca8f4cda01bc10afbf
miho <minhhoan119@gmail.com>
2021-08-16 15:05:25 +0700

    Initial Commit

---
+++ Calculator(VIPER)/Calculator_VIPER_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_VIPER_App.swift
+//  Calculator(VIPER)
+//
+//  Created by Admin on 16/08/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_VIPER_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator(VIPER)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator(VIPER)
+//
+//  Created by Admin on 16/08/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
